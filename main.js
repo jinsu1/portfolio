b1aeff64a7d3dcb3dbe133ae4f4b7f4243b9baa6
jinsu1 <jinsu4205@gmail.com>
2021-02-10 12:57:46 +0900

    JS : navbar menu scroll

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// 스크롤시 navbar의 색이 나타남
+// 스크롤시 navbar색깔 투명해제
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -9,4 +9,18 @@ document.addEventListener('scroll', () => {
     } else{
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// navbar메뉴 클릭시 스크롤 이동
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) =>{
+const target= event.target;
+const link= target.dataset.link;
+if (link == null) {
+    return;
+}
+console.log(event.target.dataset.link);
+const scrollTo= document.querySelector(link);
+scrollTo.scrollIntoView({behavior: 'smooth'});
+
 });
