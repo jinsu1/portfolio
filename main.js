9d36662f9ba97f0639c1daf77b9aa9c7e9871874
jinsu1 <jinsu4205@gmail.com>
2021-02-11 19:08:51 +0900

    JS : Home transparent

--- main.js
+++ main.js
@@ -28,9 +28,35 @@ const contactMeBtn = document.querySelector('.home__contact');
 contactMeBtn.addEventListener('click', (event) => {
     scrollIntoView('#contact');
 });
+    
 
+
+
+
+// 스크롤시 home 점차 투명 
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+ 
+});
+
+
+
+
+// 스크롤 이동힘수
 function scrollIntoView(selector) {
     const scrollTo= document.querySelector(selector);
-    scrollTo.scrollIntoView({behavior:'smooth'});
+    // scrollTo.scrollIntoView({behavior:'smooth'});
 
+        // 스크롤 이동시 타이틀이 잘리는현상 방지
+        const scrollMove = document.querySelector(selector);
+        const top = scrollMove.offsetTop - navbarHeight < 0 
+            ? 0:scrollMove.offsetTop - navbarHeight;
+        const left = scrollMove.offsetTop;
+        window.scrollTo({
+            top:top,
+            left:left,
+            behavior:'smooth'
+        });
 }
